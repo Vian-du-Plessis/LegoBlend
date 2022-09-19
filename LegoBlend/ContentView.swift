7c6c788184c6f2de39a2f1fb50cb6350ec00f1aa
Vian du Plessis <21100483@virtualwindow.co.za>
2022-09-19 18:01:31 +0200

    Initial Commit

---
+++ LegoBlend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LegoBlend
+//
+//  Created by Vian du Plessis on 2022/09/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LegoBlend/LegoBlendApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegoBlendApp.swift
+//  LegoBlend
+//
+//  Created by Vian du Plessis on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct LegoBlendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
