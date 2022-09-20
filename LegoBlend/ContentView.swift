1df67489c9741c697b9be9e3ea0273ac9036d84c
Vian du Plessis <21100483@virtualwindow.co.za>
2022-09-21 00:57:32 +0200

    Splash Screen Animation Tweaked

--- LegoBlend/ContentView.swift
+++ LegoBlend/ContentView.swift
@@ -28,72 +28,79 @@ struct ContentView: View {
         Logo(name: "n"),
         Logo(name: "d")
     ]
+    let blocks = [
+        Logo(name: "green_lego"),
+        Logo(name: "orange_lego"),
+        Logo(name: "blue_lego"),
+        Logo(name: "red_lego")
+    ]
+    @State var xValue = 0.0
+    @State var yValue = 0.0
     var body: some View {
+        ZStack {
+            ForEach(1..<20) {_ in
+                Image(blocks[Int.random(in: 0..<blocks.count)].name)
+                    .frame(width: 20, height: 20)
+                    .scaleEffect(Double.random(in: 1...2))
+                    .position(x: scaleAnimation ? (Double.random(in: -1...3) * 500) : (Double.random(in: -3...3) * 200), y: scaleAnimation ? (Double.random(in: -1...5) * 500) : (Double.random(in: -3...3) * 100))
+                    .animation(.easeInOut.speed(0.15).repeatForever(autoreverses: true), value: scaleAnimation)
+            }
 
-        
-        VStack {
-        
-            HStack {
-                Image("tr_splash")
-                    .scaleEffect(scaleAnimation ? Double.random(in: 1...1.5) : 1)
-                    .animation(.easeInOut.speed(0.6).repeatForever(), value: scaleAnimation)
-            }
-            .frame(maxWidth: .infinity, alignment: .trailing)
-            
-            GeometryReader { proxy in
-                Image("floating_lg")
-                    .frame(width: 10, height: 10)
-                    .onAppear {
-                        print(CGPoint(x: proxy.size.width, y: 0))
-                    }
-                
-            }
-            
-//            Image("floating_lg")
-//                .frame(width: 30, height: 30)
-//                .position(x: scaleAnimation ? CGPointMake(CGFloat(arc4random_uniform(UInt32(viewWidth))), 0) : 0, y: scaleAnimation ? 0 : 0)
-            
-            Spacer()
-          
-            HStack {
-                ForEach(firstWord) { item in
-                    Image(item.name)
-                        .offset(x: isExploded ? (Double.random(in: -1...1) * 500) : 0, y: isExploded ? (Double.random(in: -1...1) * 500) : 0)
-                        .opacity(isExploded ? 0 : 1)
-                        .scaleEffect(isExploded ? 13 : 1)
-                        .animation(.easeInOut.speed(0.3), value: isExploded)
-                        .scaleEffect(scaleAnimation ? Double.random(in: 1...1.2) : 1 )
-                        .animation(.easeInOut.speed(0.6).repeatForever(autoreverses: true), value: scaleAnimation)
-                }
-            }
-            .padding(15)
-            
-            HStack {
-                ForEach(secondWord) { item in
-                    Image(item.name)
-                        .offset(x: isExploded ? (Double.random(in: -1...1) * 500) : 0, y: isExploded ? (Double.random(in: -1...1) * 500) : 0)
-                        .opacity(isExploded ? 0 : 1)
-                        .scaleEffect(isExploded ? 13 : 1)
-                        .animation(.easeInOut.speed(0.3), value: isExploded)
-                        .scaleEffect(scaleAnimation ? Double.random(in: 1...1.5) : 1 )
+            VStack {
+                HStack {
+                    Image("tr_splash")
+                        .scaleEffect(scaleAnimation ? Double.random(in: 1...1.5) : 1)
                         .animation(.easeInOut.speed(0.6).repeatForever(), value: scaleAnimation)
                 }
-            }
-            .padding(15)
-            .onAppear {
-                scaleAnimation = true
+                .frame(maxWidth: .infinity, alignment: .trailing)
+//              First HStack - Top Right
+
+                Spacer()
+              
+                HStack {
+                    ForEach(firstWord) { item in
+                        Image(item.name)
+                            .offset(x: isExploded ? (Double.random(in: -1...1) * 500) : 0, y: isExploded ? (Double.random(in: -1...1) * 500) : 0)
+                            .opacity(isExploded ? 0 : 1)
+                            .scaleEffect(isExploded ? 13 : 1)
+                            .animation(.easeInOut.speed(0.3), value: isExploded)
+                            .scaleEffect(scaleAnimation ? Double.random(in: 1...1.2) : 1 )
+                            .animation(.easeInOut.speed(0.6).repeatForever(autoreverses: true), value: scaleAnimation)
+                    }
+                }
+                .padding(15)
+//              Second HStack - Logo
+                
+                HStack {
+                    ForEach(secondWord) { item in
+                        Image(item.name)
+                            .offset(x: isExploded ? (Double.random(in: -1...1) * 500) : 0, y: isExploded ? (Double.random(in: -1...1) * 500) : 0)
+                            .opacity(isExploded ? 0 : 1)
+                            .scaleEffect(isExploded ? 13 : 1)
+                            .animation(.easeInOut.speed(0.3), value: isExploded)
+                            .scaleEffect(scaleAnimation ? Double.random(in: 1...1.5) : 1 )
+                            .animation(.easeInOut.speed(0.6).repeatForever(), value: scaleAnimation)
+                    }
+                }
+                .padding(15)
+                .onAppear {
+                    scaleAnimation = true
+
+                }
+//              Third HStack - Logo
+                
+                Spacer()
+                
+                HStack {
+                    Image("bl_splash")
+                        .scaleEffect(scaleAnimation ? Double.random(in: 1...1.5) : 1)
+                        .animation(.easeInOut.speed(0.6).repeatForever(), value: scaleAnimation)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+//              Fourth HStack - Bottom Left
+                
 
             }
-            
-            Spacer()
-            
-            HStack {
-                Image("bl_splash")
-                    .scaleEffect(scaleAnimation ? Double.random(in: 1...1.5) : 1)
-                    .animation(.easeInOut.speed(0.6).repeatForever(), value: scaleAnimation)
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
         }
-
     }
 }
