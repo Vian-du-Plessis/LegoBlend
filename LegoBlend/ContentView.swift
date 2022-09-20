046f7bc06bf7b5dc0c3d22c7697d505b4ed7a462
Vian du Plessis <90850796+Vian-du-Plessis@users.noreply.github.com>
2022-09-20 16:20:08 +0200

    Splash Screen Animation

--- LegoBlend/ContentView.swift
+++ LegoBlend/ContentView.swift
@@ -7,11 +7,79 @@
 
 import SwiftUI
 
+struct Logo : Identifiable{
+   let id = UUID()
+   let name: String
+}
+
 struct ContentView: View {
+    @State private var isExploded = false
+    let firstWord = [
+        Logo(name: "L"),
+        Logo(name: "E"),
+        Logo(name: "G"),
+        Logo(name: "O")
+    ]
+    let secondWord = [
+        Logo(name: "B"),
+        Logo(name: "l_small"),
+        Logo(name: "E"),
+        Logo(name: "n"),
+        Logo(name: "d")
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(firstWord) { item in
+                Image(item.name)
+                    .offset(x: isExploded ? (Double.random(in: -1...1) * 500) : 0, y: isExploded ? (Double.random(in: -1...1) * 500) : 0)
+                    .opacity(isExploded ? 0 : 1)
+                    .scaleEffect(isExploded ? 13 : 1)
+                    .animation(.easeInOut.speed(0.3), value: isExploded)
+            }
+        }
+        
+        HStack {
+            ForEach(secondWord) { item in
+                Image(item.name)
+                    .offset(x: isExploded ? (Double.random(in: -1...1) * 500) : 0, y: isExploded ? (Double.random(in: -1...1) * 500) : 0)
+                    .opacity(isExploded ? 0 : 1)
+                    .scaleEffect(isExploded ? 13 : 1)
+                    .animation(.easeInOut.speed(0.3), value: isExploded)
+            }
+        }        
+        Button("hey") {
+            isExploded.toggle()
+        }
     }
+//        VStack {
+//            Spacer()
+//            ZStack {
+//                ForEach(0..<75) { _ in
+//                    Rectangle()
+//                        .rotation(Angle(degrees: Double.random(in: 0..<360)))
+//                        .frame(width: 40, height: 20)
+//                        .foregroundColor(.indigo)
+//                        .offset(x: isExploded ? (Double.random(in: -1...1) * 500) : 0, y: isExploded ? (Double.random(in: -1...1) * 500) : 0)
+//                        .opacity(isExploded ? 0 : 1)
+//                        .animation(.easeInOut.speed(0.3), value: isExploded)
+//                        .padding()
+//                }
+//                Rectangle()
+//                    .frame(width: 300, height: 300)
+//                    .foregroundColor(.indigo)
+//                    .opacity(isExploded ? 0 : 1)
+//                    .animation(.easeInOut.speed(0.3), value: isExploded)
+//                    .padding()
+//                Spacer()
+//
+//                Button("Hey") {
+//                    isExploded.toggle()
+//                }
+//            }
+//        }
+//        Text("Hello, world!")
+//            .padding()
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
